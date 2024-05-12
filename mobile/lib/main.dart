82812bd4ccce1398cbdf8476751c91a209572d7e
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2024-05-12 04:41:21 -0300

    feat: app v2 with two pages

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,23 +1,5 @@
-// import 'package:flutter/material.dart';
-// import 'screens/welcome_screen.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       home: WelcomeScreen(),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'screens/tasks_screen.dart';
 import 'screens/welcome_screen.dart';
 import 'blocs/task_bloc.dart';
 import 'services/task_service.dart';
@@ -33,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       home: BlocProvider(
         create: (context) => TaskBloc(taskService: TaskService()),
-        child: TasksScreen(),
+        child: WelcomeScreen(),
       ),
     );
   }
--- mobile/lib/screens/welcome_screen.dart
+++ mobile/lib/screens/welcome_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../blocs/task_bloc.dart';
 import 'tasks_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -11,8 +13,16 @@ class WelcomeScreen extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => TasksScreen()),
+            // Continua capturando o TaskBloc antes de usar o Navigator
+            final taskBloc = BlocProvider.of<TaskBloc>(context, listen: false);
+
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => BlocProvider.value(
+                  value: taskBloc,
+                  child: TasksScreen(),
+                ),
+              ),
             );
           },
           child: Text("Ir para Tarefas"),
